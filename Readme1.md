83179e22e60e9b173f8d772224d75ca283f10495
Juno Brown <amarbrown@gmail.com>
2021-01-09 16:32:22 -0500

    added function needed to generate inquirer questions and answers

---
+++ Readme1.md
@@ -0,0 +1,13 @@
+ # Title
+Readme-Generator
+## Purpose
+To create an app that creates a high quality readme from prompts
+## Usage
+
+NPM Node.js inquirer
+## Contributors 
+undefined
+## Contact
+www.github.com/marquez-brown
+www.linkedin.com/marquezbrown
+
--- index.js
+++ index.js
@@ -1,13 +1,84 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+// const questions = [];
+const writeFileAsync = util.promisify(fs.writeFile);
 
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      message: 'what is the title of your project?',
+      name: 'title',
+      
+    },
+    {
+      type: 'input',
+      message: 'what is the purpose of your project?',
+      name: 'purpose',
+      
+    },
+    {
+      type: 'input',
+      message: 'what modules need to be installed to run this app?',
+      name: 'usage',
+      
+    },
+    {
+      type: 'input',
+      message: 'who were your contributors?',
+      name: 'contributor',
+      
+    },
+    {
+      type: 'input',
+      message: 'Enter your GitHub Username',
+      name: 'github',
+      
+    },
+    {
+      type: 'input',
+      message: 'Enter your LinkedIn URL.',
+      name: 'linkedin',
+      
+    },
+  ]);
+};
+const generateReadme = (answers) => 
+` #${answers.title}
+
+## Purpose
+
+${answers.purpose}
+
+## Usage
+
+${answers.usage}
+
+## Contributors 
+
+${answers.contributors}
+
+## Contact
+
+${answers.github}
+${answers.linkedin}
+
+##License
+${generateMarkdown.}
+`
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile("README1.md", ) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+// function init() {}
 
-// Function call to initialize app
-init();
+// Function call to initialize app;
+
+promptUser()
+.then((answers) => writeFileAsync('Readme1.md', generateReadme(answers)))
+.then(() => console.log('Successfully wrote to Readme1.md'))
+.catch((err) => console.error(err));
